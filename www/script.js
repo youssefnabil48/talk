2884a6867b7292ee18e879d749c64bab26fcb9c1
joe <youssefnabil48@outlook.com>
2021-11-21 18:25:55 +0200

    add working turn server

--- www/script.js
+++ www/script.js
@@ -1,21 +1,13 @@
 /* globals App, io, cabin*/
 const ICE_SERVERS = [
 	{ urls: "stun:stun.l.google.com:19302" },
-	{ urls: "stun:stun.stunprotocol.org:3478" },
-	{ urls: "stun:stun.sipnet.net:3478" },
-	{ urls: "stun:stun.ideasip.com:3478" },
-	{ urls: "stun:stun.iptel.org:3478" },
-	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
 	{
 		urls: [
-			"turn:173.194.72.127:19305?transport=udp",
-			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-			"turn:173.194.72.127:443?transport=tcp",
-			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
+			"turn:ec2-35-180-101-49.eu-west-3.compute.amazonaws.com",
 		],
-		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
-		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
-	},
+		username: "username1",
+		credential: "key1"
+	}
 ];
 
 const APP_URL = (() => {
